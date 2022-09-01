b53a1cbfe818a8d47a7c6ca149f15c55ea4d5ff2
hernanfda <hernanfda@hotmail.com>
2022-09-01 16:05:39 -0300

    Uploading to github

---
+++ README.md
@@ -0,0 +1,6 @@
+RANDOM COLOR PICKER
+
+Just a random color picker made with JS.
+Topics:
+-Basic DOM manipulation
+-Math object
---
+++ app.js
@@ -0,0 +1,16 @@
+const body = document.querySelector('body')
+const btn = document.querySelector('button');
+const h1 = document.querySelector('h1');
+
+btn.addEventListener('click', function () {
+    const newColor = makeRandomColor()
+    body.style.backgroundColor = newColor
+    h1.innerText = newColor
+})
+
+const makeRandomColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r},${g},${b})`;
+}
